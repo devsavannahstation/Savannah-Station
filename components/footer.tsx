608f14c4424c65a622c49650aa4898db8e056591
devsavannahstation <dev.savannahstation@gmail.com>
2026-03-06 23:38:49 +0530

    fixed redirect issue

--- components/footer.tsx
+++ components/footer.tsx
@@ -114,7 +114,7 @@ export default function Footer() {
               >
                 <Link
                   href={
-                    "https://www.instagram.com/theunion.savannahstation/?utm_source=ig_embed&amp%3Butm_campaign=loading%22"
+                    "https://www.instagram.com/theunion.savannahstation/"
                   }
                 >
                   <Instagram className="w-4 h-4 " />
